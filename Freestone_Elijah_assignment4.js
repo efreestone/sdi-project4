e0e6ebdaf49ea25ac70b690d83035a494bf43a40
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2012-09-18 15:34:42 -0600

    checkNumber simplified. Added checkEmail function.

--- Freestone_Elijah_assignment4.js
+++ Freestone_Elijah_assignment4.js
@@ -10,10 +10,9 @@ var say = function(message) { console.log(message); }; //Console.log Function
 var myLibrary = function() {
     
     var checkNumber = function(phoneNum) { //Problem 1. Check valid phone number string
-        var testNum = phoneNum;
-        var pattern = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-            if (pattern.test(testNum)) {
-                var validNumber = testNum.replace(pattern, "($1) $2-$3");
+        var numPattern = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+            if (numPattern.test(phoneNum)) {
+                var validNumber = phoneNum.replace(numPattern, "($1) $2-$3");
                     say("This is a valid Phone Number. "); //Console Log if a Valid Number
                         return true;
             } else {
@@ -22,11 +21,25 @@ var myLibrary = function() {
             };
     };
     
+    var checkEmail = function(email) { //Problem 2. Check valid email address string
+        var emailPattern = /^[a-zA-Z0-9.-_+]+@[a-zA-Z0-9.-_+]+.[a-zA-Z]{2,4}$/ //`~!@#$%^&*()={[}]|\:;"'<,>.?\/\
+            if (emailPattern.test(email)) {
+                say("This is a valid email address. "); //Console Log if a valid email address
+                    return true;
+            } else {
+                say("Please enter a valid email address. "); //Console Log if not a valid email address
+                    return false;
+            }
+    };
+    
     return { //returns for all functions
-        "checkNumber": checkNumber
+        "checkNumber": checkNumber, //Problem 1
+        "checkEmail":  checkEmail //Problem 2
     };
 };
 
-var newLib = new myLibrary();
+var newLib = new myLibrary(); //Creating new myLibrary to keep original intact when modifying.
 
-say(newLib.checkNumber("(970)988-8190")); //Problem 1. Check valid phone number call
+say(newLib.checkNumber("(970)988-8190")); //Problem 1 call. Check valid phone number.
+
+say(newLib.checkEmail("e=+fre/^@2@kdj=+.com")); //Problem 2 call. Check valid email address.
